c81520920cf9cd366f7e73efbd48c15efc330c0b
BuildTools <unconfigured@null.spigotmc.org>
2019-09-15 14:52:54 -0400

    finalized

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,39 +3,13 @@
 
 #include "Common.h"
 
-
-/*
- *	enum EventType 
- *      PROCESS_ARRIVAL,
- *      PROCESS_EXIT,
- *      PROCESS_ARRIVE_CPU,
- *      PROCESS_FINISH_CPU,
- *      PROCESS_ARRIVE_DISK1,
- *      PROCESS_ARRIVE_DISK2,
- *      PROCESS_FINISH_DISK1,
- *      PROCESS_FINISH_DISK2,
- *      SIMULATION_FINISH,
- *	*/
-
-
 int main() {
 
 	sim.enqueue(PROCESS_ARRIVAL);
-	//sim.enqueue(PROCESS_ARRIVAL);
-	//sim.enqueue(PROCESS_ARRIVAL);
+	sim.enqueue(PROCESS_ARRIVAL);
+	sim.enqueue(PROCESS_ARRIVAL);
 
 	sim.start();
-
-/*
- * Queue priority debug
-	sim.debug(sim.eventQueue.top().time);
-	sim.eventQueue.pop();
-	sim.debug(sim.eventQueue.top().time);
-    sim.eventQueue.pop();
-	sim.debug(sim.eventQueue.top().time);
-    sim.eventQueue.pop();
-	exit(0);
-*/
 
 	while(sim.running) {
 		sim.process_from_queue();
